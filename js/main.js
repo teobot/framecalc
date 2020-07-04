53127ba41c25b05ee291bb589b70a7ea31ac72a0
teobot <theoclapperton@outlook.com>
2020-07-04 17:03:52 +0100

    last commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var main = new Vue({
     data: {
         inputFrame: 0,
         framesMeasure: 0,
+        totalFrames: 0,
+        videoLength: 0,
     },
     created() {
         console.log("Created!");
@@ -18,9 +20,19 @@ var main = new Vue({
             if (isNaN(total)) {
                 html_return = `Waiting for Inputs...`;
             } else {
-                html_return = `<small>${this.inputFrame}fps @ ${this.framesMeasure} Frames is </small><strong class="blue-text" style="text-decoration: underline;">${total}s</strong><small> long.</small>`;
+                html_return = `<small>@ ${this.inputFrame}fps, ${this.framesMeasure} Frames is </small><strong class="blue-text" style="text-decoration: underline;">${total}s</strong><small> long.</small>`;
             }
             return html_return;
-        }
+        },
+        totalFramesInVideo: function() {
+            var total = (this.totalFrames / this.videoLength);
+            var html_return_fps;
+            if (total == 0) {
+                html_return_fps = `Waiting for Inputs...`;
+            } else {
+                html_return_fps = `<small>${this.totalFrames} frames over ${this.videoLength}s is </small><strong class="blue-text" style="text-decoration: underline;">${total}fps</strong><small></small>`;
+            }
+            return html_return_fps;
+        },
     }
-})
+});
